1927bd1590e7c1c6f0ba85b69fc32800da9c5857
Priyam Roy <itspriyam.roy21@gmail.com>
2025-04-25 02:19:59 +0530

    Implement search bar with full background image and updated heading

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Body.css';
+import SearchBar from './Searchbar';
+
+
+function Body() {
+  return (
+    <div className="body-container">
+      <SearchBar />
+    </div>
+  );
+}
+
+export default Body;
